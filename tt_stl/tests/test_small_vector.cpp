c391adfc28aadebbff43b3ad1b2f32a1fa5359e7
Bryan Wilder Field Lozano <blozano@tenstorrent.com>
2025-12-31 14:12:51 -0800

    Update tt_stl/tests/test_small_vector.cpp

--- tt_stl/tests/test_small_vector.cpp
+++ tt_stl/tests/test_small_vector.cpp
@@ -279,8 +279,9 @@ TEST_F(SmallVectorIntTest, ForwardAndReverseIterators) {
     Vec vec{1, 2, 3, 4};
     // Sum elements via forward iterators.
     int sum = 0;
-    for (int& it : vec) {
-        sum += it;
+    // NOLINTNEXTLINE(modernize-loop-convert)
+    for (auto* it = vec.begin(); it != vec.end(); ++it) {
+        sum += *it;
     }
     EXPECT_EQ(sum, 10);
     // Copy into a std::vector via const iterators.
